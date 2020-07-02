2747ea2346ca1a8c47708d84d21aaaed33059bc2
yellowtides <andi.dicu12@gmail.com>
2020-07-03 02:43:43 +0300

    replaced hardcoded hash with chosen password hash

--- operations/verify_key.php
+++ operations/verify_key.php
@@ -1,8 +1,7 @@
 <?php
     function is_key_valid($string) {
-        // $stored_hash = file_get_contents('../key.txt');
-        $stored_hash = '$2y$10$VYaE1scc7h0dqrJwU8/fHu/ckWIPey4c5EYtr0EVkwliCdJ5DRT1e';
-        return password_verify($string, $stored_hash);
+        $stored_hash = file('key.txt', FILE_IGNORE_NEW_LINES);
+        return password_verify($string, $stored_hash[0]);
         // check password against the stored hash
     }
 ?>
