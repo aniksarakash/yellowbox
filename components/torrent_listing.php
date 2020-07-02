88e8fafd42853820a11b95680582dba75a337d69
yellowtides <andi.dicu12@gmail.com>
2020-07-02 22:07:20 +0300

    separated torrent cards

--- components/torrent_listing.php
+++ components/torrent_listing.php
@@ -6,12 +6,19 @@
     <div class="torrents">
 	<?php
 		$out = array();
+        $line_count = 0;
+
         exec("deluge-console info", $out);
-        echo '<div class="torrent_info"><pre>';
 		foreach($out as $line) {
-			echo "$line\n\n";
+            echo "$line\n\n";
+            ++$line_count;
+            if ($line_count % 9 == 1) {
+                echo '<div class="torrent_info"><pre>';
+            }
+            if ($line_count % 9 == 0) {
+                echo '</pre></div>';
+            }
 		}
-        echo '</pre></div>';
 	?>
     </div>
 </body>
