7ce62824c7212a14513d5761754ba4f05f7e1f1b
Andi <andi.dicu12@gmail.com>
2020-07-03 12:25:28 +0100

    migrated to apache2

--- boot.sh
+++ boot.sh
@@ -7,21 +7,18 @@ fi
 if ! ps ax | grep -v "grep" | grep "/usr/bin/deluged" > /dev/null
 then
     # makes sure deluged is running
-    /usr/bin/deluged &
+    sudo /usr/bin/deluged &
     # sets the download directory as the ./storage folder
-    deluge-console config -s download_location $PWD/storage &
+    sudo deluge-console config -s download_location $PWD/storage &
 fi
 
-if ! ps ax | grep -v "grep" | grep "php -S localhost:17772" > /dev/null 
-then
-    # makes sure php server is running on port 17772
-    sudo php -S localhost:17772 &
-fi
+# restarts the apache2 server on the default port
+sudo /etc/init.d/apache2 restart &
 
 passwd=$(php -r "echo password_hash(\"$1\", PASSWORD_DEFAULT);")
 
-echo "$passwd" > "./key.txt" 
-echo "Your password is now $1. Don't forget it!"
-echo "Its salted hash ($passwd) has been stored in key.txt as plaintext."
+sudo echo "$passwd" > "./key.txt" 
+
+printf "\n\nYour password is now $1. Don't forget it!\nIts salted hash ($passwd) has been stored in key.txt as plaintext.\n\n"
 
 exit
