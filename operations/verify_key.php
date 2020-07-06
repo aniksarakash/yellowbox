11d1f1d19082649a3748ab1fb7125483c72877ed
yellowtides <andi.dicu12@gmail.com>
2020-07-06 15:34:56 +0300

    changed location of hash from text file to system environment

--- operations/verify_key.php
+++ operations/verify_key.php
@@ -1,7 +1,7 @@
 <?php
     function is_key_valid($string) {
         $stored_hash = getenv('SEEDBOX_PASSWD');
-        return password_verify($string, $stored_hash[0]);
+        return password_verify($string, $stored_hash);
         // check password against the stored hash
     }
 ?>
